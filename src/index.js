9fe182e1aac7140481350624b0423acfadbf417a
ekodhani <ekonurramadhani@gmail.com>
2020-07-16 14:59:43 +0700

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import NavbarComponent from './sidenav/NavbarComponent';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <NavbarComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/sidenav/NavbarComponent.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+class NavbarComponent extends React.Component {
+    componentDidMount() {
+        let sidenav = document.querySelector('#slide-out');
+        M.Sidenav.init(sidenav, {});
+    }
+
+    render() {
+        return (
+            <div>
+                <nav>
+                    <a href="#" data-target="slide-out" class="sidenav-trigger show-on-large"><i class="material-icons">menu</i></a>
+                    <div className="container">
+                        <ul id="nav-mobile" class="right hide-on-med-and-down">
+                            <li><a href="">Beranda</a></li>
+                            <li><a href="">Profile</a></li>
+                        </ul>
+                    </div>
+                </nav>
+
+                <ul id="slide-out" className="sidenav">
+                    <li><div className="user-view">
+                        <div className="background">
+                            <img src="https://materializecss.com/images/office.jpg"></img>
+                        </div>
+                        <a href="#user"><img className="circle" src="https://ekodhani.github.io/images/user/dhaniaqua.jpg"></img></a>
+                        <a href="#name"><span className="white-text name">Dhaniflw</span></a>
+                        <a href="#email"><span className="white-text email">ekonurramadhani@gmail.com</span></a>
+                    </div></li>
+                    <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
+                    <li><a href="#!">Second Link</a></li>
+                    <li><div className="divider"></div></li>
+                    <li><a className="subheader">Subheader</a></li>
+                    <li><a className="waves-effect" href="#!">Third Link With Waves</a></li>
+                </ul>
+
+            </div>
+        );
+    }
+}
+
+export default NavbarComponent;
